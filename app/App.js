1c72b2a39b9111f79abfa0a437dc8fc257b6a798
rkiel <cygnus@pobox.com>
2015-03-13 12:33:44 -0600

    added index.html

---
+++ app/App.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div className="container">
+      </div>
+    )
+  }
+});
+
+React.render(
+  <App />,
+  document.getElementById('app')
+)
