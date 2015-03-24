70ae0be8c11fd6fbb2a4dad78bdddeac66651304
rkiel <cygnus@pobox.com>
2015-03-23 21:43:22 -0400

    bball

--- app/Basketball.js
+++ app/Basketball.js
@@ -1,27 +1,17 @@
 var React       = require('react');
-var Month       = require('./components/Month');
-var DateHelper  = require('./utils/DateHelper');
-var TimeActions = require('./actions/TimeActions');
+var Basketball  = require('./basketball/Basketball');
 
-var startDate   = new Date(); // new Date(2016,1);
-var dateHelper  = new DateHelper(startDate);
-var daysInMonth = dateHelper.lastDate();
-var startDay    = 1;
-var endDay      = 31;
-
-TimeActions.start(startDate,startDay,endDay);
-
-var Basketball = React.createClass({
+var App = React.createClass({
   render: function(){
     return (
       <div className="container">
-        <Month year={ startDate.getFullYear() } month={ startDate.getMonth() } startDay={ startDay } endDay={ endDay } />
+        <Basketball />
       </div>
     )
   }
 });
 
 React.render(
-  <Basketball />,
+  <App />,
   document.getElementById('app')
 )
